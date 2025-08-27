f5bf4a57d7ea201adfd495f7fe6d5f257575d86e
PARAM AHLAWAT <68227187+JAATMOBIL@users.noreply.github.com>
2025-08-28 03:22:24 +0530

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,37 @@
+//MARIO-LESS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char input[100];
+    int n = 0;
+    while (1)
+    {
+        printf("No of lines: ");
+        if (fgets(input, sizeof(input), stdin) != NULL)
+        {
+            
+            if (sscanf(input, "%d", &n) == 1 && n > 0)
+            {
+                break; 
+            }
+        }
+        printf("Please enter a positive integer greater than 0.\n");
+    }
+
+    for (int r = 1; r <= n; r++)
+    { 
+        for (int s= 0; s <n - r; s++)
+        {
+            printf(" ");
+        }
+        for (int t = 0; t< r; t++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
